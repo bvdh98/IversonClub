6f6ac07ae278c8346261dac0046e5f645170eafe
Ben <bvdhest1998@gmail.com>
2022-01-25 15:56:36 -0800

    replaced hard-coded config variables with environment variables and deleted fireBaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyA9LP7TtaLTFr6J-MYbvXX14Sjqyy4tzAM",
-    authDomain: "iversonclub.firebaseapp.com",
-    projectId: "iversonclub",
-    storageBucket: "iversonclub.appspot.com",
-    messagingSenderId: "52006038861",
-    appId: "1:52006038861:web:ec6caef8198a5fac005b04",
-    measurementId: "G-338PY91TPX"
-  };
